5abec9078974141eea72b1355c5c687ea9e241fd
Gabriel Alves Pereira <145914573+Gab-Zag@users.noreply.github.com>
2026-03-06 01:36:50 -0300

    feat:add base project

---
+++ src/main/java/com/fetin2026/whtie_label/WhtieLabelApplication.java
@@ -0,0 +1,13 @@
+package com.fetin2026.whtie_label;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhtieLabelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhtieLabelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fetin2026/whtie_label/WhtieLabelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fetin2026.whtie_label;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhtieLabelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
